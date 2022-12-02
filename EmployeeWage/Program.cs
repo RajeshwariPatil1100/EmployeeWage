faccad2ff5df12935d6c0ec747cfb4608beee73f
RajeshwariPatil1100 <rajeshwaripatil1100@gmail.com>
2022-12-02 13:17:57 +0530

    Rajeshwari : Employee attendence check

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fullTime = 1;
+            Random randomObj = new Random();
+
+            int checkPresent = randomObj.Next(0, 2);
+            Console.WriteLine("Random Number is " + checkPresent);
+
+            if (checkPresent == fullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
