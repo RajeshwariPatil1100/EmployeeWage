dabfb1848d87b40ee9c0b142b7f72aaadce44caa
RajeshwariPatil1100 <rajeshwaripatil1100@gmail.com>
2022-12-08 14:05:06 +0530

    [Rajeshwari] add Part Time

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -5,23 +5,28 @@ namespace EmployeeWage
     internal class Program
     {
         static void Main(string[] args)
+        {
+            int fullTime = 1, partTime = 2, Emp_Rate_Per_Hrs = 20, empHrs = 0, empWage = 0;
+            Random randomObj = new Random();
+            int checkPresent = randomObj.Next(0, 3);
+            Console.WriteLine("Random Number is " + checkPresent);
+            if (checkPresent == fullTime)
             {
-                int fullTime = 1, Emp_Rate_Per_Hrs = 20, empHrs = 0, empWage = 0;
-                Random randomObj = new Random();
-                int checkPresent = randomObj.Next(0, 2);
-                Console.WriteLine("Random Number is " + checkPresent);
-                if (checkPresent == fullTime)
-                {
-                    empHrs = 8;
-                    Console.WriteLine("Employee is Present");
-                }
-                else
-                {
-                    empHrs = 0;
-                    Console.WriteLine("Employee is Absent");
-                }
-                empWage = empHrs * Emp_Rate_Per_Hrs;
-                Console.WriteLine("Emp Wage :" + empWage);
+                empHrs = 8;
+                Console.WriteLine("Employee is Present");
             }
+            else if (checkPresent == partTime)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                empHrs = 0;
+                Console.WriteLine("Employee is Absent");
+            }
+            empWage = empHrs * Emp_Rate_Per_Hrs;
+            Console.WriteLine("Emp Wage :" + empWage);
+        }
     }
 }
